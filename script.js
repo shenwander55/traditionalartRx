913f1e3485394092d184b15b7a7bd310095a79b4
shenwander55 <cyanite0815@gmail.com>
2024-07-30 10:03:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 document.getElementById('quizForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    let senior = document.querySelector('input[name="senior"]:checked').value;
-    let preference = document.querySelector('input[name="preference"]:checked').value;
+    let q1 = document.querySelector('input[name="q1"]:checked').value;
+    let q2 = document.querySelector('input[name="q2"]:checked').value;
+    let q3 = document.querySelector('input[name="q3"]:checked').value;
+    let q4 = document.querySelector('input[name="q4"]:checked').value;
+    let q5 = document.querySelector('input[name="q5"]:checked').value;
 
     let suggestion = '';
 
-    if (senior === 'yes') {
+    if (q5 === 'yes') {
         suggestion = '建議兌換2張入園門票';
+    } else if (q4 === 'yes') {
+        suggestion = '建議兌換1張入園門票+1張手作券';
     } else {
-        if (preference === 'crafting') {
+        if (q2 === 'crafting') {
             suggestion = '建議兌換1張入園門票+1張手作券';
-        } else if (preference === 'exhibiting') {
+        } else if (q2 === 'exhibiting') {
             suggestion = '建議兌換2張入園門票';
         }
     }
